e4fc9abec6cc067c56e2488b542a6ed68649b5ec
Sangwon <born9507@snu.ac.kr>
2020-08-21 15:00:02 +0900

    Update soupOnly.py, main.py

--- src/main.py
+++ src/main.py
@@ -5,18 +5,19 @@ from bs4 import BeautifulSoup
 
 driver = webdriver.Chrome('./chromedriver')
 
-#3초 기다린다
+# 3초 기다린다
 driver.implicitly_wait(3)
 
 driver.get('https://otrade.co/contest/60')
 
+# 시작 시간 기록
 startTime = time.time()
-print('스크롤링 시작')
 
 while True:
     try:
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        if time.time() - startTime > 60:
+        # 60초동안 아래로 스크롤링
+        if time.time() - startTime > 120:
             break
     except KeyboardInterrupt:
         print(f"소요시간: {time.time()-startTime}")
@@ -27,16 +28,51 @@ while True:
 html = driver.page_source
 soup = BeautifulSoup(html, 'html.parser')
 
-with open("test.html", 'w') as f:
+# html 파일로 export
+with open("page.html", 'w') as f:
     f.write(soup.prettify())
 
+time.sleep(5)
+############################################################################################################33
 
-#class item 중에서 
-# span class="name_text_cut" 이 제목
-# span class="amount" > span class="pink_txt" 가 모집금액
+# soup 처리 과정
+with open("page.html") as f:
+    soup = BeautifulSoup(f, 'html.parser')
 
-#id_1382 > span > span.cont > span.name.text_cut
-#id_1428 > span > span.cont > span.amount > span.pink_txt
+titles = soup.select('span > span.cont > span.text_cut')
+amounts = soup.select('span > span.cont > span.amount > span.pink_txt')
+links = soup.select('a[href]')
 
-# with open("test.html", 'w') as f:
-#     f.write(html)
+
+#id_1
+
+# a href="javascript:fnstartupPopup
+
+# var url = "/contest/"+season_id+"/"+startup_id+"/popup";
+
+print(len(titles))
+print(len(amounts))
+
+titles = list(map(lambda title: title.text.strip() ,titles))
+amounts = list(map(lambda amount: 
+    int(amount.text.replace('원','').replace(',','').strip()),  
+    amounts 
+))
+
+results = {}
+
+for i in range(len(titles)):
+    title = titles[i]
+    # title = euc2utf(title)
+    amount = amounts[i]
+
+    results[title] = amount
+
+# key 값에 따라 내림차순으로 정렬
+results = sorted(results.items(), key=lambda x: x[1], reverse=True)
+
+# 
+with open('result.txt', 'w') as f:
+    for i in range(len(results)):
+        data = (f"{i+1}, {results[i][0]}, {results[i][1]}원, \n")
+        f.write(data)
--- src/soupOnly.py
+++ src/soupOnly.py
@@ -1,13 +1,26 @@
 from bs4 import BeautifulSoup
+import csv
 
-with open("test.html") as f:
+def euc2utf(x):
+    return str(x.encode('utf-8'))
+
+with open("page.html") as f:
     soup = BeautifulSoup(f, 'html.parser')
 
 titles = soup.select('span > span.cont > span.text_cut')
 amounts = soup.select('span > span.cont > span.amount > span.pink_txt')
+links = soup.select('a[href]')
+
+
+#id_1
+
+# a href="javascript:fnstartupPopup
+
+# var url = "/contest/"+season_id+"/"+startup_id+"/popup";
 
 print(len(titles))
 print(len(amounts))
+print(len(links))
 
 titles = list(map(lambda title: title.text.strip() ,titles))
 amounts = list(map(lambda amount: 
@@ -15,11 +28,35 @@ amounts = list(map(lambda amount:
     amounts 
 ))
 
+results = {}
+
+for i in range(len(titles)):
+    title = titles[i]
+    # title = euc2utf(title)
+    amount = amounts[i]
+
+    results[title] = amount
+
+# key 값에 따라 내림차순으로 정렬
+results = sorted(results.items(), key=lambda x: x[1], reverse=True)
+
+# 
+with open('result.txt', 'w') as f:
+    for i in range(len(results)):
+        data = (f"{i+1}, {results[i][0]}, {results[i][1]}원, \n")
+        f.write(data)
 
 
+# sorted(d.items(), key=lambda x: x[1], reverse=True)
+
+# with open('result.csv','w', newline='') as f:
+#     w = csv.writer(f)
+#     w.writerows(result.keys())
+#     w.writerows(result.values())
 
 # span class="name_text_cut" 이 제목
 # span class="amount" > span class="pink_txt" 가 모집금액
+
 
 #id_1382 > span > span.cont > span.name.text_cut
 #id_1428 > span > span.cont > span.amount > span.pink_txt
